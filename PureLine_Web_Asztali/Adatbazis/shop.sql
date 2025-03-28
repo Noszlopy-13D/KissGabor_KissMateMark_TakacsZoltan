80c093163b417e02e9c27f41d693b32c08cd584b
Kissg22 <103967397+Kissg22@users.noreply.github.com>
2025-03-28 09:59:25 +0100

    update 03.28.

--- PureLine_Web_Asztali/Adatbazis/shop.sql
+++ PureLine_Web_Asztali/Adatbazis/shop.sql
@@ -204,3 +204,21 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Termék kategóriához rendelése
+ALTER TABLE `products`
+ADD CONSTRAINT `fk_products_category`
+FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Termék hozzáadó felhasználóhoz kötése
+ALTER TABLE `products`
+ADD CONSTRAINT `fk_products_user`
+FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Rendelés vásárlóhoz rendelése
+ALTER TABLE `orders`
+ADD CONSTRAINT `fk_orders_customer`
+FOREIGN KEY (`customer_id`) REFERENCES `users`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
